d3f1fda4bb287e59d06ad7eb9b0a06833f481803
maxwesel <max@toralarm.com>
2025-02-20 22:47:00 +0100

    init

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.7
 import PackageDescription
 // Debug valdity of the package with: swift package resolve
 let package = Package(
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LogDog",
-            url: "https://github.com/modrena/log-dog-ios-sdk/releases/download/v1.1.3/log-dog-ios-sdk.xcframework.zip",
-            checksum: "e34a5af08bbcc00d7e90f7fbfdd45fbec8ab5a667ce403bfee87396cb2c1d16a"
+            url: "https://github.com/modrena/log-dog-ios-sdk/releases/download/v1.1.5/log-dog-ios-sdk.xcframework.zip",
+            checksum: "8901c3a1bbcb51ad08b41b1ea6ee056433f43956166d79a964542126ca41a15e"
         ),
     ]
 )
