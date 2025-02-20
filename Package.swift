ee10bad7f781dd3ff6634826bdd063d4c7ca21cc
maxwesel <max@toralarm.com>
2025-02-20 01:08:46 +0100

    init

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LogDog",
-            url: "https://github.com/modrena/log-dog-ios-sdk/releases/download/v1.0.7/log-dog-ios-sdk.xcframework.zip",
-            checksum: "8959b17a08a9d7230eaf96a068b0cd38118800dd3c5cc0afaab01131e2ec8b4b"
+            url: "https://github.com/modrena/log-dog-ios-sdk/releases/download/v1.0.8/log-dog-ios-sdk.xcframework.zip",
+            checksum: "afef9b8c2cc29d24b78d29c716e8f011cf76572ed0f1effa7309fabb6d5310a8"
         ),
     ]
 )
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# log-dog-ios-sdk (v1.0.7)
+# log-dog-ios-sdk (v1.0.8)
 
 ## Installation
 
 ```swift
 dependencies: [
-    .package(url: "https://github.com/modrena/log-dog-ios-sdk.git", from: "1.0.6")
+    .package(url: "https://github.com/modrena/log-dog-ios-sdk.git", from: "1.0.8")
 ]
 ```
 
