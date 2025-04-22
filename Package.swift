0af19dcb8cd56d8dfa8c87963aab68d015f15f6d
maxwesel <max@toralarm.com>
2025-04-22 14:42:45 +0200

    Update version to 1.4.427

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LogDog",
-            url: "https://github.com/modrena/log-dog-ios-sdk/releases/download/v1.4.426/log-dog-ios-sdk.xcframework.zip",
-            checksum: "e6af4f4b000f27e3ec529437dddd4c0ebea79d765e99aed8c46ea95df9a88310"
+            url: "https://github.com/modrena/log-dog-ios-sdk/releases/download/v1.4.427/log-dog-ios-sdk.xcframework.zip",
+            checksum: "2fb508679a8c0684aaf122a9b5cea843ddff1abf110ac266c2cedbd2b3da477e"
         ),
     ]
 )
